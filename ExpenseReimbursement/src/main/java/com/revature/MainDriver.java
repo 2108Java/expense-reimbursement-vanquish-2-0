f329c68815dbf5bc3621ab68ed3d898f0fa681ee
Jymm Allexzydd Enriquez <jymm.enriquez@gmail.com>
2021-09-24 14:04:20 -0500

    Added mini tutorial in MainDriver and comments in pom.xml

--- ExpenseReimbursement/src/main/java/com/revature/MainDriver.java
+++ ExpenseReimbursement/src/main/java/com/revature/MainDriver.java
@@ -1,10 +1,26 @@
 package com.revature;
 
+import io.javalin.Javalin;
+
 public class MainDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Javalin app = Javalin.create().start(9000);
+		
+		
+		//Open postman and copy this! ---> http://localhost:9000/ <----
+		//And paste it in the GET input box in Postman
+		//Just uncomment this one if you want to try your connection to Postman
+		
+//				Click the line and press Ctrl + Shift + C
+//									|
+//									|
+//									|
+//									v		
+//		app.get("/", ctx -> ctx.json("Connecting to Postman!"));
+		
+		
 	}
 
 }
