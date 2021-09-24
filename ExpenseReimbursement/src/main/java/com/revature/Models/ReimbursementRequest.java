7436ad25bad34cbbb291765af34ef37ef8d6614c
Jymm Allexzydd Enriquez <jymm.enriquez@gmail.com>
2021-09-24 14:12:52 -0500

    Added Users and Reimbursement Models

---
+++ ExpenseReimbursement/src/main/java/com/revature/Models/ReimbursementRequest.java
@@ -0,0 +1,88 @@
+package com.revature.Models;
+
+public class ReimbursementRequest {
+	
+	private int id;
+	private String reimbursementType;
+	private double amount;
+	private String description;
+	private String status;
+	private String timeOfRequest;
+	private String username;
+	
+	public ReimbursementRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ReimbursementRequest(int id, String reimbursementType, double amount, String description, String status,
+			String timeOfRequest, String username) {
+		super();
+		this.id = id;
+		this.reimbursementType = reimbursementType;
+		this.amount = amount;
+		this.description = description;
+		this.status = status;
+		this.timeOfRequest = timeOfRequest;
+		this.username = username;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getReimbursementType() {
+		return reimbursementType;
+	}
+
+	public void setReimbursementType(String reimbursementType) {
+		this.reimbursementType = reimbursementType;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getTimeOfRequest() {
+		return timeOfRequest;
+	}
+
+	public void setTimeOfRequest(String timeOfRequest) {
+		this.timeOfRequest = timeOfRequest;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+
+}
---
+++ ExpenseReimbursement/src/main/java/com/revature/Models/Users.java
@@ -0,0 +1,47 @@
+package com.revature.Models;
+
+public class Users {
+	
+	private String username;
+	private String password;
+	private boolean isFinanceManager;
+	
+	public Users() {
+		super();
+		
+	}
+
+	public Users(String username, String password, boolean isFinanceManager) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.isFinanceManager = isFinanceManager;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isFinanceManager() {
+		return isFinanceManager;
+	}
+
+	public void setFinanceManager(boolean isFinanceManager) {
+		this.isFinanceManager = isFinanceManager;
+	}
+	
+	
+
+}
