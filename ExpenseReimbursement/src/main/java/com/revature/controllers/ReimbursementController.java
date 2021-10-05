1adc5afb72c26240f0bd0dc6567c5dadeb77dfbf
polyglotarist <majid.samir2@gmail.comgit config --global user.email majid.samir2@gmail.com>
2021-10-05 07:01:06 -0700

    initial

--- ExpenseReimbursement/src/main/java/com/revature/controllers/ReimbursementController.java
+++ ExpenseReimbursement/src/main/java/com/revature/controllers/ReimbursementController.java
@@ -1,24 +1,87 @@
 package com.revature.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+
+import com.revature.models.ReimbursementRequest;
 
 import io.javalin.http.Context;
 
 public class ReimbursementController {
+	
+	private List<ReimbursementRequest>  reimbursementList = new ArrayList<>();
+	
 
 	public void initalizeList() {
 		// TODO Auto-generated method stub
 		
+		
+		reimbursementList.add(
+				new ReimbursementRequest(
+						1, 
+						"travel",
+						1000, 
+						"Plane ticket",
+						"10-01-2021",
+						"pending",
+						"username1"
+				)
+				
+		);
+		
+		reimbursementList.add(
+				new ReimbursementRequest(
+						1, 
+						"food",
+						50, 
+						"3 meals",
+						"10-01-2021",
+						"pending",
+						"username1"
+				)
+				
+		);
+		
+		reimbursementList.add(
+				new ReimbursementRequest(
+						2, 
+						"food",
+						1000, 
+						"Plane ticket",
+						"10-01-2021",
+						"pending",
+						"username1"
+				)
+				
+		);
+		
+		reimbursementList.add(
+				new ReimbursementRequest(
+						3, 
+						"lodging",
+						100, 
+						"Hotel",
+						"10-01-2021",
+						"pending",
+						"username1"
+				)
+				
+		);
+		
+		
 	}
 
-	public CompletableFuture<?> getReimbursement(Context ctx) {
+	public ReimbursementRequest getReimbursement(Context ctx) {
 		// TODO Auto-generated method stub
-		return null;
+		return this.reimbursementList.get(0);
 	}
 
-	public CompletableFuture<?> getAllPlanets(Context ctx) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<ReimbursementRequest> getAllRequests(Context ctx) {
+		
+		
+		ctx.res.setStatus(200);
+		return this.reimbursementList;
 	}
 
 }
--- ExpenseReimbursement/src/main/resources/public/Reimbursement.js
+++ ExpenseReimbursement/src/main/resources/public/Reimbursement.js
@@ -2,14 +2,40 @@
  * 
  */
  
- console.log("why isnt this working??");
- console.log("test from reimbursement");
+// console.log("test from reimbursement");
+
+//this is the ajax portion:
  
  function getRequest(){
 	
 }
 
+
+let button = document.getElementById("requestSubmit");
+
+button.addEventListener('click', getAllRequests);
+
+
 function getAllRequests(){
+	let ReimbursementUrl = "http://localhost:9000/Reimbursements";
+	
+	let xhttp = new XMLHttpRequest();
+	
+	
+	xhttp.open("Get", ReimbursementUrl);
+	
+	xhttp.send();
+	
+	xhttp.onreadystatechange = function(){
+		
+		if(this.readyState == 4 && this.status == 200){
+			
+			let reimbursementArray = JOSON.parse(this.respnseText);
+			
+			console.log(reimbursementArray);
+		}
+	}
+	
 	
 }
 
